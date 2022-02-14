e87e3624c83f93da1dc4079edd5f01e8437f6977
Davit9494 <davitsahakyan94@mail.ru>
2022-02-14 09:58:09 +0400

    fix: changed animation duration

--- js/svg2.js
+++ js/svg2.js
@@ -1,8 +1,8 @@
 const beautiful_svg = new Vivus("beautiful_svg", {
-    type: "oneByOne",
-    duration: 100,
-    reverseStack: true,
-  });
-  function playBeautifulAnim() {
-    beautiful_svg.stop().reset().play();
-  }
+  type: "sync",
+  duration: 100,
+  reverseStack: true,
+});
+function playBeautifulAnim() {
+  beautiful_svg.stop().reset().play();
+}
